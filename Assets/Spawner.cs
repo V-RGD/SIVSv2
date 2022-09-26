51c8e3a86ce2702664511fd69a20a369379ef417
anaturu <a.marotine@rubika-edu.com>
2022-09-26 19:12:50 +0200

    ajout prefab player

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject enemy;
+    public GameObject player;
+    private Vector3 vPlayer;
+    
     void Start()
     {
         
@@ -13,6 +16,13 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        vPlayer = player.transform.position;
+        StartCoroutine(spawner());
+    }
+
+    IEnumerator spawner()
+    {
+        yield return new WaitForSeconds(1);
+        Instantiate(enemy, new Vector3(Random.Range(vPlayer.x -5,vPlayer.x +5), Random.Range(vPlayer.y -5,vPlayer.y +5), 0), Quaternion.identity);
     }
 }
