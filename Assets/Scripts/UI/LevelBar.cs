03890345c9a6aff790ea106a35c44c48da495764
DJ_Yazid <y.djaoui@rubika-edu.com>
2022-09-27 11:56:18 +0200

    HealthBar & EnnemisBar

---
+++ Assets/Scripts/UI/LevelBar.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+    void Start()
+    {
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+
+    public void SetMaxLevel(float level)
+    {
+        slider.maxValue = level;
+        slider.value = level;
+
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void SetLevel(float level)
+    {
+        slider.value = level;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
---
+++ Assets/Scripts/UI/Yazid_Script.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Yazid_Script : MonoBehaviour
+{
+
+    // Health
+    public HealthBar health;
+    public float currentHealth;
+
+    // Experience
+    public LevelBar level;
+    public float currentlevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = health.GetComponent<Slider>().value;
+        currentlevel = level.GetComponent<Slider>().value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Health
+        currentHealth = health.GetComponent<Slider>().value;
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            currentHealth -= 10;
+            health.GetComponent<HealthBar>().SetHealth(currentHealth);
+        }
+
+        // Experience
+        currentlevel = level.GetComponent<Slider>().value;
+
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            currentlevel += 10;
+            level.GetComponent<LevelBar>().SetLevel(currentlevel);
+        }
+    }
+}
