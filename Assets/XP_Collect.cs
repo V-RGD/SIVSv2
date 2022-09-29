18a02198a51be3f5c761a4aa5924ac800d43c33a
DJ_Yazid <y.djaoui@rubika-edu.com>
2022-09-29 23:03:22 +0200

    Associer les boules de XP au bar

--- Assets/XP_Collect.cs
+++ Assets/XP_Collect.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class XP_Collect : MonoBehaviour
 {
@@ -15,12 +16,21 @@ public class XP_Collect : MonoBehaviour
     private float playerDist;
 
     private bool canAttract;
+
+    private GameObject GreenBar;
+    private GameObject YellowBar;
+    private GameObject OrangeBar;
+    private GameObject RedBar;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         rb = GetComponent<Rigidbody2D>();
+        GreenBar = GameObject.Find("LevelBar1");
+        YellowBar = GameObject.Find("LevelBar2");
+        OrangeBar = GameObject.Find("LevelBar3");
+        RedBar = GameObject.Find("LevelBar4");
     }
 
     // Update is called once per frame
@@ -42,18 +52,22 @@ public class XP_Collect : MonoBehaviour
             if (xpType == 0)
             {
                 gameManager.greenXP++;
+                GreenBar.GetComponent<LevelBar>().BoostLevel(20);
             }
             if (xpType == 1)
             {
                 gameManager.yellowXP++;
+                YellowBar.GetComponent<LevelBar>().BoostLevel(20);
             }
             if (xpType == 2)
             {
                 gameManager.orangeXP++;
+                OrangeBar.GetComponent<LevelBar>().BoostLevel(20);
             }
             if (xpType == 3)
             {
                 gameManager.redXP++;
+                RedBar.GetComponent<LevelBar>().BoostLevel(20);
             }
             Destroy(gameObject);
         }
