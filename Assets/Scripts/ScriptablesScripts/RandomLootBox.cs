253d2da279ded166f948e0a743fd1226bcd294af
DJ_Yazid <y.djaoui@rubika-edu.com>
2022-09-29 16:37:37 +0200

    ...

--- Assets/Scripts/ScriptablesScripts/RandomLootBox.cs
+++ Assets/Scripts/ScriptablesScripts/RandomLootBox.cs
@@ -16,7 +16,7 @@ public class RandomLootBox : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      //  PanelLootBox.SetActive(false);
+        PanelLootBox.SetActive(false);
         name = gameObject.transform.GetChild(0);
         description = gameObject.transform.GetChild(1);
     }
